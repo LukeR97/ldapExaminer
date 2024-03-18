5f97b74de792d5cba92d44ee029cd9c3405e7a3e
Luke <rothwell.luke@gmail.com>
2024-03-18 12:02:39 +0000

    add analyze func

--- src/main/java/com/app/Ldap.java
+++ src/main/java/com/app/Ldap.java
@@ -1,7 +1,12 @@
 package com.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.unboundid.ldap.sdk.Attribute;
 import com.unboundid.ldap.sdk.LDAPConnection;
 import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.SearchResultEntry;
 
 public class Ldap {
     
@@ -19,6 +24,25 @@ public class Ldap {
             System.out.println(searchResult.getSearchEntries().get(0));
             if(searchResult.getEntryCount() > 0){
                 return searchResult;
+            }
+        } catch (LDAPException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<Attribute> analyzeLDAP(com.unboundid.ldap.sdk.SearchResultEntry attribs, LDAPConnection ldapConnection){
+        List<Attribute> results = new ArrayList<>();
+        String uidNum = attribs.getAttributeValue("uidNumber");
+        try{
+            com.unboundid.ldap.sdk.SearchResult searchResult = ldapConnection.search("ou=people,dc=example,dc=com", com.unboundid.ldap.sdk.SearchScope.SUB, "(uidNumber=" + uidNum + ")", "uid");
+            //System.out.println(searchResult.getSearchEntries().get(0));
+            if(searchResult.getEntryCount() > 1){
+                //results.add(searchResult.getSearchEntries().get(1).toString());
+                for (SearchResultEntry entry: searchResult.getSearchEntries()){
+                    results.add(entry.getAttribute("uid"));
+                }
+                return results;
             }
         } catch (LDAPException e){
             e.printStackTrace();
